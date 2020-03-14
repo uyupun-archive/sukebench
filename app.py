b18169007743f2b9c10b8bca1bd6b89b4c2ef86f
Kazuki Tobita <kazukiti201@gmail.com>
2020-03-14 22:23:40 +0900

    Add cpu info

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
+import psutil
+
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
+def fetch_cpu_info():
+    cpu_info = {
+        'real_count': psutil.cpu_count(logical=False),
+        'logical_count': psutil.cpu_count(),
+        'use_rate': psutil.cpu_percent(),
+        'frequency': psutil.cpu_freq().current / 1000,
+        'load_average': psutil.getloadavg(),
+    }
+    return cpu_info
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', cpu_info = fetch_cpu_info())
 
 if __name__ == '__main__':
     app.run(debug=True)
