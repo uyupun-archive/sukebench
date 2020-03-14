8a9315a01c11b9e2d7ca151c519e8afd6021e6f3
Kazuki Tobita <kazukiti201@gmail.com>
2020-03-14 19:25:37 +0900

    Add templates

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-import flask
+from flask import Flask, render_template
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hello, world!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug = False, host = '0.0.0.0', port = 5000)
+    app.run(debug=True)
