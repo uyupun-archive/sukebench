ad828ece6f53eb3e9ae90207f17d10667b935cd0
Kazuki Tobita <kazukiti201@gmail.com>
2020-03-14 23:33:03 +0900

    Add disks info

--- app.py
+++ app.py
@@ -32,9 +32,27 @@ def fetch_swap_info():
     }
     return swap_info
 
+def fetch_disks_info():
+    disks_info = {
+        'partitions': psutil.disk_partitions(),
+        'total': psutil.disk_usage(path='/').total / 1_000_000_000,
+        'free': psutil.disk_usage(path='/').free / 1_000_000_000,
+        'used': psutil.disk_usage(path='/').used / 1_000_000_000,
+        'use_rate': psutil.disk_usage(path='/').percent,
+        'read_count': '{:,d}'.format(psutil.disk_io_counters().read_count),
+        'write_count': '{:,d}'.format(psutil.disk_io_counters().write_count),
+        'read_bytes': '{:,d}'.format(psutil.disk_io_counters().read_bytes),
+        'write_bytes': '{:,d}'.format(psutil.disk_io_counters().write_bytes),
+    }
+    return disks_info
+
 @app.route('/')
 def index():
-    return render_template('index.html', cpu_info = fetch_cpu_info(), memory_info = fetch_memory_info(), swap_info = fetch_swap_info())
+    return render_template('index.html',
+                           cpu_info = fetch_cpu_info(),
+                           memory_info = fetch_memory_info(),
+                           swap_info = fetch_swap_info(),
+                           disks_info = fetch_disks_info())
 
 if __name__ == '__main__':
     app.run(debug=True)
